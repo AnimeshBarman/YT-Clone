de62a8d04c4666a5230a1ce5e1ca010698502eae
Animesh Barman <barmananimesh635@gmail.com>
2024-04-17 08:31:02 +0530

    .

--- src/controllers/healthCheck.controller.js
+++ src/controllers/healthCheck.controller.js
@@ -4,6 +4,10 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 
 const healthcheck = asyncHandler(async (req, res) => {
+
+    return res.status(200).json(
+        new apiResponse(200, {}, "Health is fine")
+    )
     //TODO: build a healthcheck response that simply returns the OK status as json with a message
 })
 
